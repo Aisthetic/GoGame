c5a13c49d2948d1d4de5b7222d5d8f5e0657ddd9
Testoxe <47027479+Testoxe@users.noreply.github.com>
2019-01-27 19:32:06 +0000

    Cleanness Added

--- menu.c
+++ menu.c
@@ -19,6 +19,7 @@ void printLogo(){
 
 int MainMenu(){
     int choice = 0;
+    FILE* rules = NULL ;
     printLogo();
     printf("                            ****************************************************************\n");
     printf("                            *     *                                                        *\n");
@@ -26,35 +27,64 @@ int MainMenu(){
     printf("                            *     *                                                        *\n");
     printf("                            ****************************************************************\n");
     printf("                            *     *                                                        *\n");
-    printf("                            *  2  *                  Charger partie                        *\n");
+    printf("                            *  2  *                       Aide                             *\n");
     printf("                            *     *                                                        *\n");
     printf("                            ****************************************************************\n");
     printf("                            *     *                                                        *\n");
-    printf("                            *  3  *                       Aide                             *\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            ****************************************************************\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            *  4  *                      Quitter                           *\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            ****************************************************************\n");
-    //Choix du mode de  jeu
-    printf("                            ****************************************************************\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            *  1  *                   Homme VS Homme                       *\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            ****************************************************************\n");
-    printf("                            *     *                                                        *\n");
-    printf("                            *  2  *                  Homme VS Machine                      *\n");
+    printf("                            *  3  *                      Quitter                           *\n");
     printf("                            *     *                                                        *\n");
     printf("                            ****************************************************************\n");
     scanf("%d",&choice);
-    return choice;
+    switch(choice){
+        case 2:
+            rules = fopen("rules.txt","r"); //On ouvre le fichier aide
+            int caractereActuel = 0;
+            if (rules != NULL){
+            // On peut lire le fichier
+            // Boucle de lecture des caractères un à un
+            do{
+                caractereActuel = fgetc(rules); // On lit le caractère
+                printf("%c", caractereActuel); // On l'affiche
+            } while (caractereActuel != EOF);
+
+
+            }
+            else
+            {
+            // On affiche un message d'erreur si on veut
+            printf("Erreur lors de l'ouverture");
+            }
+            system("pause");
+            fclose(rules); //on ferme le fichier
+            system("cls");
+            break;
+        case 3 :
+            exit(0);
+            break;
+        case 1 :
+            //Choix du mode de  jeu
+            printf("                            ****************************************************************\n");
+            printf("                            *     *                                                        *\n");
+            printf("                            *  1  *                   Homme VS Homme                       *\n");
+            printf("                            *     *                                                        *\n");
+            printf("                            ****************************************************************\n");
+            printf("                            *     *                                                        *\n");
+            printf("                            *  2  *                  Homme VS Machine                      *\n");
+            printf("                            *     *                                                        *\n");
+            printf("                            ****************************************************************\n");
+            scanf("%d",&choice);
+            return choice;
+            break;
+        default :
+            system("cls");
+            break;
+    }
 }
 
 int PVEMenu(){
         printf("                            ****************************************************************\n");
         printf("                            *     *                                                        *\n");
-        printf("                            *  !  *             Choisissez une difficult�                  *\n");
+        printf("                            *  !  *             Choisissez une difficulte                  *\n");
         printf("                            *     *                                                        *\n");
         printf("                            ****************************************************************\n");
         int difficulty;
